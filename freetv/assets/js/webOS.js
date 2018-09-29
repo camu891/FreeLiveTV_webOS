f8892b5e2fa6f9ebc90b93223f6381fd643ff935
Matias Camusso <mati_camusso@hotmail.com>
2018-09-29 16:44:18 -0300

    Fixed icon CN

--- freetv/assets/js/webOS.js
+++ freetv/assets/js/webOS.js
@@ -1,58 +1,87 @@
 
   //sample code for calling LS2 API
- var lunaReq= webOS.service.request("luna://com.palm.systemservice",
- {
- 	method:"clock/getTime",
- 	parameters:{},
- 	onSuccess: function (args) {
- 		console.log("UTC:", args.utc);
- 	},
- 	onFailure: function (args) {
- 	}
+  var lunaReq= webOS.service.request("luna://com.palm.systemservice",
+  {
+    method:"clock/getTime",
+    parameters:{},
+    onSuccess: function (args) {
+     console.log("UTC:", args.utc);
+   },
+   onFailure: function (args) {
+   }
  });
 
-function keyboardVisibilityChange(event) {
-	var visibility = event.detail.visibility;
-	if(visibility){
-		console.log("Virtual keyboard appeared");
+  function keyboardVisibilityChange(event) {
+   var visibility = event.detail.visibility;
+   if(visibility){
+    console.log("Virtual keyboard appeared");
         // Do something.
-    }
-    else{
-    	console.log("Virtual keyboard disappeared");
+      }
+      else{
+       console.log("Virtual keyboard disappeared");
         // Do something.
+      }
     }
-}
-document.addEventListener('keyboardStateChange', keyboardVisibilityChange, false);
+    document.addEventListener('keyboardStateChange', keyboardVisibilityChange, false);
 
-function cursorVisibilityChange(event) {
-	var visibility = event.detail.visibility;
-	if(visibility){
-		Log("Cursor appeared");
+    function cursorVisibilityChange(event) {
+     var visibility = event.detail.visibility;
+     if(visibility){
+      Log("Cursor appeared");
         // Do something.
-    }
-    else{
-    	Log("Cursor disappeared");
+      }
+      else{
+       Log("Cursor disappeared");
         // Do something.
+      }
     }
-}
-document.addEventListener('cursorStateChange', cursorVisibilityChange, false);
+    document.addEventListener('cursorStateChange', cursorVisibilityChange, false);
 
-function load() {
-	document.addEventListener("cursorStateChange", onCursor, false);
-	document.addEventListener("visibilitychange", function () { Log(document.visibilityState);}, false);
-	window.addEventListener("blur", function() { Log("Focus off");}, false);
-	window.addEventListener("focus", function() { Log("Focus on");}, false);
-}
-
-function onCursor(event) {
-	if (event.detail.visibility)
-            Log("Cursor on");
-       else
-            Log("Cursor off");
+    function load() {
+     document.addEventListener("cursorStateChange", onCursor, false);
+     document.addEventListener("visibilitychange", function () { Log(document.visibilityState);}, false);
+     window.addEventListener("blur", function() { Log("Focus off");}, false);
+     window.addEventListener("focus", function() { Log("Focus on");}, false);
    }
 
-   var line = 0;
-   function Log( msg ) {
-   	console.log(msg);
+   function onCursor(event) {
+     if (event.detail.visibility)
+      Log("Cursor on");
+    else
+      Log("Cursor off");
+  }
+
+  var line = 0;
+  function Log( msg ) {
+    console.log(msg);
    	//document.getElementById('log').innerHTML += ++line + ": " + msg + "<br>";
    }
+
+  //backbutton
+
+  /*window.addEventListener("keydown", function(inEvent){   
+    if(window.event) {
+      keycode = inEvent.keyCode;
+    }
+    if (keycode==461) {doBack();}
+  });
+
+
+  function doBack() {
+    // Do something for Back button();
+
+    // If you need to open Home UI
+    if (someCondition == true)
+      webOS.platformBack();
+  }*/
+
+  window.addEventListener('popstate', function (event) {
+    var data = event.state;
+    if (data) {
+        // do page move or anything else
+        console.log("vuelve");
+      } else {
+        webOS.platformBack();
+      }
+      
+    }, false);
