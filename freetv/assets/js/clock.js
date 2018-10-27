4f7911e0a95cff9ab155411cb6d74a905bec4d30
Matias Camusso <mati_camusso@hotmail.com>
2018-10-27 18:13:18 -0300

    CAmbio formato fecha, agrego programas

--- freetv/assets/js/clock.js
+++ freetv/assets/js/clock.js
@@ -1,4 +1,7 @@
 
+
+var months    = ['Enero','Febrero','Marzo','Abril','Mayo','Junio','Julio','Agosto','Septiembre','Octubre','Noviembre','Diciembre'];
+
 function getdate(){
   var today = new Date();
   var h = today.getHours();
@@ -6,10 +9,11 @@ function getdate(){
   var s = today.getSeconds();
   var day = today.getDay();
   var month = today.getMonth();
+  var thisMonth = months[month];
   var year = today.getFullYear();
 
 
-  $("span.date").text((day<10 ? '0'+day : day)+"/"+(month<10 ? '0'+month : month)+"/"+year);    
+  $("span.date").text((day<10 ? '0'+day : day)+" de "+thisMonth+" de "+year);    
   $("span.clock").text(h+":"+(m<10 ? '0'+m : m)+":"+(s<10 ? '0'+s : s));
 
   setTimeout(function(){getdate()}, 500);
